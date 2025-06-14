945d6a70aa6364999a1352416d3d4ffbf9ff8250
Mario Uță <marioernest818@gmail.com>
2025-06-14 13:02:07 +0300

    docker image

---
+++ front_end/vite.config.js
@@ -0,0 +1,8 @@
+// front_end/vite.config.js
+export default {
+  root: '.',               // base directory
+  build: {
+    outDir: 'dist',        // output directory
+    emptyOutDir: true,
+  },
+};
